5fae590a8f49b29c6bd2b95e9e84042072b0b9ce
Marc Lübke <marc.luebke@gal-digital.de>
2019-12-12 12:15:50 +0100

    a

--- src/js/server/sockets.js
+++ src/js/server/sockets.js
@@ -5,27 +5,30 @@ import config from '../config';
 
 export default () => {
   if (!config.settings.socket) {
-    config.settings.socket = io('ws://172.17.2.156:8080');
+    config.settings.socket = io('ws://172.17.2.156:8080', () => {
+      config.settings.socket.on('winner', (data) => {
+        endGame(data.message);
+      });
 
-    config.settings.socket.on('winner', (data) => {
-      endGame(data.message);
+      config.settings.socket.on('fieldClick', (data) => {
+        checkField(data.player, config.settings.field[data.x][data.y]);
+      });
+
+      config.settings.socket.on('spectate', (data) => {
+        spectate(data.fields);
+      });
+
+      config.settings.socket.on('generatefields', (data) => {
+        generateFields(data.length);
+      });
+
+      config.settings.socket.on('updateStatus', (data) => {
+        updateStatus(data.message);
+      });
     });
 
-    config.settings.socket.on('fieldClick', (data) => {
-      checkField(data.player, config.settings.field[data.x][data.y]);
-    });
-
-    config.settings.socket.on('spectate', (data) => {
-      spectate(data.fields);
-    });
-
-    config.settings.socket.on('generatefields', (data) => {
-      generateFields(data.length);
-    });
-
-    config.settings.socket.on('updateStatus', (data) => {
-      updateStatus(data.message);
-    });
+    config.settings.socket.emit('startgame', { length: config.settings.fields, player1: config.settings.player1 });
+    return;
   }
 
   config.settings.socket.emit('startgame', { length: config.settings.fields, player1: config.settings.player1 });
--- src/js/start.js
+++ src/js/start.js
@@ -9,14 +9,14 @@ export default () => {
     event.preventDefault();
     const { target } = event;
 
+    config.setSetting('clicked', 0);
+    config.setSetting('fields', target[2].value);
     config.setSetting('inputBlocked', false);
     config.setSetting('isPlayer1', true);
-    config.setSetting('clicked', 0);
     config.setSetting('player1', target[0].value);
     config.setSetting('player2', target[1].value);
-    config.setSetting('fields', target[2].value);
-    config.setSetting('mode', target[3].value);
     config.setSetting('maxFields', parseInt(target[2].value, 10) * parseInt(target[2].value, 10));
+    config.setSetting('mode', target[3].value);
 
     if (target[3].value === '3') {
       server();
